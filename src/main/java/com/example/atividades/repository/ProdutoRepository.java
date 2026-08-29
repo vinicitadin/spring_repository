9096193ee3e4a492cc8b2c0a0b733d571acc68a7
Vinicius Ferreira Citadin <viniciuscitadin@gmail.com>
2026-08-29 20:11:23 -0300

    remove referência não utilizada

--- src/main/java/com/example/atividades/repository/ProdutoRepository.java
+++ src/main/java/com/example/atividades/repository/ProdutoRepository.java
@@ -2,7 +2,6 @@ package com.example.atividades.repository;
 
 import com.example.atividades.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
